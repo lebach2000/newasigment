0d8de11c891c2aadcc68a6015be67791f9b20ed1
lebach2000 <kenrou.hol0.the.wolf@gmail.com>
2019-11-06 16:22:08 +0700

    uwp

---
+++ asaigment/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using ass_uwp_0611.Entity;
+using ass_uwp_0611.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace asaigment
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<ass_uwp_0611.Entity.Contact> listContact;
+        private ContactModel _model;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this._model = new ContactModel();
+        }
+
+        private void AddContact_OnClick(object sender, RoutedEventArgs e)
+        {
+            _model.Insert(new Contact
+            {
+                Name = this.Name.Text,
+                PhoneNumber = long.Parse(this.PhoneNumber.Text)
+            });
+            DisplayListContact();
+        }
+
+        private void SearchContact_OnClick(object sender, RoutedEventArgs e)
+        {
+            var list = _model.GetContactByName(this.KeyWord.Text);
+            listContact = new ObservableCollection<Contact>();
+            if (list == null || list.Count == 0)
+            {
+                return;
+            }
+
+            listContact = list;
+            ListViewContact.ItemsSource = listContact;
+        }
+
+        private void DisplayListContact()
+        {
+            var list = _model.GetListContacts();
+            listContact = new ObservableCollection<Contact>();
+            if (list == null || list.Count == 0)
+            {
+                return;
+            }
+
+            listContact = list;
+            ListViewContact.ItemsSource = listContact;
+        }
+    }
+}
